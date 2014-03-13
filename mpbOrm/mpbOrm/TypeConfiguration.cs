db8dd204ce9fbde1998f68dfa635ae80d9cc4218
Bernhard Johannessen <bernhard@voytsje.com>
2014-03-13 03:14:02 +0000

    Added classes for building configurations to override default conventions

---
+++ mpbOrm/mpbOrm/TypeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mpbOrm
+{
+    public class TypeConfiguration
+    {
+        public string TableName { get; private set; }
+    }
+}
