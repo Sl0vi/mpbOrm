034ca1bd03bbb2a3b8fb0ca1bf40e91c23c9d080
Bernhard Johannessen <bernhard@voytsje.com>
2014-03-20 05:28:43 +0000

    Fixed build error

--- mpbOrm/mpbOrm/RepositoryBase.cs
+++ mpbOrm/mpbOrm/RepositoryBase.cs
@@ -26,6 +26,7 @@ namespace mpbOrm
     using System.Collections.Generic;
     using System.Data;
     using System.Linq;
+    using System.Reflection;
     using System.Text.RegularExpressions;
 
     /// <summary>
